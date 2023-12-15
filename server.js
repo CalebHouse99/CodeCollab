81839c0abb7c584c89a0603df7f132f20d4542d9
unknown <calebhouse99@gmail.com>
2023-12-14 22:54:13 -0600

    server.js fix

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 /* Calling in Express */
 const express = require('express');
-
-/* bring in db */
 const connectDB = require('./config/db');
+const path = require('path');
 
 /* App variable with express */
 const app = express();
@@ -13,16 +12,19 @@ connectDB();
 // Init middleware
 app.use(express.json({ extended: false }));
 
-/* Single end point, take get request to "/" and then do a req, res callback */
-app.get('/', (req, res) => res.send('API Running'));
-
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
-/* Look for env variable called PORT when deployed to herokou it will get port number OR listen locally on 5000. */
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000;
 
 /* Listen to app variable on port, added callback message once it connects */
